ebe2108fecd79b20d9c5e5e978db269216d4b2e0
Mark A. Hershberger <mah@nichework.com>
2024-07-31 13:58:02 -0400

    Introduce argparse for cli args and remove wonky dep on DOCKER_IMAGE
    * Change name to ji_grep to avoid conflict with other commands

--- src/jenkins_inventory.py
+++ src/jenkins_inventory.py
@@ -1,15 +1,19 @@
-"""Command line tools to query jenkins."""
+"""
+Command line tools to query jenkins.
+
+Right now this just provides a way (ji_grep) to search jenkins for a string.
+
+Examples
+--------
+  poetry run ji_grep example/web-ci
+"""
 
 import logging
 import os
+from argparse import ArgumentParser, Namespace
 
 from api4jenkins import Jenkins
 from py_dotenv_safe import config
-
-# Configuring logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
 
 # dotenv config
 options = {
@@ -20,18 +24,98 @@ options = {
 
 
 class JenkinsInventory:
-    """Interface for querying Jenkins"""
+    """Interface for querying Jenkins."""
 
     @classmethod
-    def grep(cls):
+    def configure_loggers(cls, verbose_level: int) -> None:
+        """
+        Standard logger config to hid messages from noisy libraries.
+
+        Parameters
+        ----------
+        verbose_level : int
+            Level of verbosity.  0 for minimal.
+        """
+        log_format = "%(asctime)s - %(levelname)s - %(message)s"
+        loggers_to_configure = [
+            "api4jenkins.http",
+            "httpx",
+            "httpcore.connection",
+            "httpcore.http11",
+            "httpcore.http2",
+            "httpcore.proxy",
+            "httpcore.socks",
+        ]
+
+        root_level = logging.INFO
+        their_level = logging.WARNING
+        if verbose_level < 1:
+            log_format = "%(message)s"
+        if verbose_level >= 1:
+            root_level = logging.DEBUG
+            their_level = logging.INFO
+        if verbose_level >= 2:
+            their_level = logging.DEBUG
+
+        # Set logging for specific modules
+        for logger_name in loggers_to_configure:
+            logging.getLogger(logger_name).setLevel(their_level)
+
+        logging.basicConfig(level=root_level, format=log_format)
+
+    @classmethod
+    def std_args(cls, parser: ArgumentParser) -> Namespace:
+        """
+        Standard arg parsing.
+
+        Parameters
+        ----------
+        parser : ArgumentParser
+            Argument handler to augment with std args.
+
+        Returns
+        -------
+        Namespace :
+            Parsed arguments.
+        """
         # Load environment variables
         config(options)
 
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            help="Show more messages (use -v for DEBUG and requests.INFO, -vv for DEBUG and requests.DEBUG)",
+            action="count",
+            default=0,
+        )
+        args = parser.parse_args()
+        cls.configure_loggers(args.verbose)
+        return args
+
+    @classmethod
+    def grep_cli(cls) -> None:
+        """UI for ji_grep."""
+        parser = ArgumentParser(description="Search for a string in Jenkins jobs")
+        parser.add_argument("search", help="The string search for")
+
+        args = cls.std_args(parser)
+
+        cls.grep(args.search)
+
+    @classmethod
+    def grep(cls, search: str) -> None:
+        """
+        Implementation of ji_grep.
+
+        Parameters
+        ----------
+        search : str
+            What to look for.
+        """
         # Retrieve configurations from .env
         jenkins_url = os.getenv("JENKINS_URL")
         username = os.getenv("JENKINS_USER")
         password = os.getenv("JENKINS_TOKEN")
-        docker_image = os.getenv("DOCKER_IMAGE")
 
         # Connect to Jenkins
         jenkins = Jenkins(jenkins_url, auth=(username, password))
@@ -42,11 +126,13 @@ class JenkinsInventory:
         for item in jenkins.iter():
             try:
                 config_xml = item.configure()
-                if docker_image and docker_image in config_xml:
+                if search and search in config_xml:
                     jobs_using_docker.append(item.url)
             except Exception as e:
-                logging.warn(f"Error accessing configuration for {item.url}: {str(e)}")
+                logging.exception(
+                    f"Error accessing configuration for {item.url}: {str(e)}"
+                )
 
         # Output results
         for job_url in jobs_using_docker:
-            print(f'Docker image "{docker_image}" is used in job at: {job_url}')
+            logging.info(f'String "{search}" is used in job at: {job_url}')
